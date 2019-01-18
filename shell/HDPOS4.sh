dcee5fcb82eb6d34a7419b080f8813af202ec870
shujun <shujun@hd123.com>
2019-01-18 13:57:28 +0800

    modify h4

--- shell/HDPOS4.sh
+++ shell/HDPOS4.sh
@@ -105,7 +105,7 @@ deploying_container() {
 													s%password=hd40%password=${CNEWORAPWD}%;\
 													s%username=hd40%username=${CNEWORAUSER}%;\
 													s%default_schema=hd40%default_schema=${CNEWORAUSER}%;\
-													s%runMode=development%runMode=production%" /opt/heading/tomcat7/webapps/${CH4IMAGE}-web/WEB-INF/classes/${CH4IMAGE}-web.properties') >/dev/null 2>&1
+													s%runMode=development%runMode=production%" /opt/heading/tomcat7/webapps/${CH4IMAGE}-web/WEB-INF/classes/hdpos4-web.properties') >/dev/null 2>&1
 }
 
 pull_image() {
